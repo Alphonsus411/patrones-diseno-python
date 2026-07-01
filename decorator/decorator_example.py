63d5c3889a90e53394183d3d85166597324e05a6
Adolfo Gonzalez Hernandez <adolfo.gonzalez@genetsis.com>
2026-07-01 09:39:48 +0200

    Creación de la carpeta /decorator y sus primeros ejemplos

---
+++ decorator/decorator_example.py
@@ -0,0 +1,63 @@
+from abc import ABC, abstractmethod
+
+
+class ICofee(ABC):
+    @abstractmethod
+    def get_cost(self):
+        pass
+
+    @abstractmethod
+    def get_description(self):
+        pass
+
+
+class SimpleCofee(ICofee):
+    def get_cost(self):
+        return 1.0
+
+    def get_description(self):
+        return "Simple cofee"
+
+
+class CoffeeDecorator(ICofee):
+    def __init__(self, coffee):
+        self._coffee = coffee
+
+    def get_cost(self):
+        return self._coffee.get_cost()
+
+    def get_description(self):
+        return self._coffee.get_description()
+
+
+class MilkDecorator(CoffeeDecorator):
+    def __init__(self, coffee):
+        super().__init__(coffee)
+
+    def get_cost(self):
+        return self._coffee.get_cost() + 0.5
+
+    def get_description(self):
+        return self._coffee.get_description() + ", milk"
+
+
+class WhippedCreamDecorator(CoffeeDecorator):
+    def __init__(self, coffee):
+        super().__init__(coffee)
+
+    def get_cost(self):
+        return self._coffee.get_cost() + 0.7
+
+    def get_description(self):
+        return self._coffee.get_description() + ", whipped cream"
+
+coffee = SimpleCofee()
+print(f"{coffee.get_description()}: ${coffee.get_cost()}")
+
+coffee = MilkDecorator(coffee)
+print(f"{coffee.get_description()}: ${coffee.get_cost()}")
+
+coffee = WhippedCreamDecorator(coffee)
+print(f"{coffee.get_description()}: ${coffee.get_cost()}")
+
+
---
+++ decorator/decorator_example_duck_typing.py
@@ -0,0 +1,50 @@
+
+
+class SimpleCofee:
+    def get_cost(self):
+        return 1.0
+
+    def get_description(self):
+        return "Simple cofee"
+
+
+class CoffeeDecorator:
+    def __init__(self, coffee):
+        self._coffee = coffee
+
+    def get_cost(self):
+        return self._coffee.get_cost()
+
+    def get_description(self):
+        return self._coffee.get_description()
+
+
+class MilkDecorator(CoffeeDecorator):
+    def __init__(self, coffee):
+        super().__init__(coffee)
+
+    def get_cost(self):
+        return self._coffee.get_cost() + 0.5
+
+    def get_description(self):
+        return self._coffee.get_description() + ", milk"
+
+
+class WhippedCreamDecorator(CoffeeDecorator):
+    def __init__(self, coffee):
+        super().__init__(coffee)
+
+    def get_cost(self):
+        return self._coffee.get_cost() + 0.7
+
+    def get_description(self):
+        return self._coffee.get_description() + ", whipped cream"
+
+coffee = SimpleCofee()
+print(f"{coffee.get_description()}: ${coffee.get_cost()}")
+
+coffee = MilkDecorator(coffee)
+print(f"{coffee.get_description()}: ${coffee.get_cost()}")
+
+coffee = WhippedCreamDecorator(coffee)
+print(f"{coffee.get_description()}: ${coffee.get_cost()}")
