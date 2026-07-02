b1bf935f776d3bc1faed70e652b73fc2ca7e13a2
Adolfo Gonzalez Hernandez <adolfo.gonzalez@genetsis.com>
2026-07-02 09:54:30 +0200

    Creación de la carpeta para el patrón command

---
+++ command/command_example.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+
+class ICommand(ABC):
+    @abstractmethod
+    def execute(self):
+        pass
+
+
+class Order:
+    def place(self):
+        print("Order has been placed.")
+
+    def cancel(self):
+        print("Order has been cancelled.")
+
+
+class PlaceOrderCommand(ICommand):
+    def __init__(self, order):
+        self.order = order
+
+    def execute(self):
+        self.order.place()
+
+
+class CancelOrderCommand(ICommand):
+    def __init__(self, order):
+        self.order = order
+
+    def execute(self):
+        self.order.cancel()
+
+
+class Waiter:
+    def take_command(self, command):
+        command.execute()
+
+
+order = Order()
+place_order = PlaceOrderCommand(order)
+cancel_order = CancelOrderCommand(order)
+
+waiter = Waiter()
+waiter.take_command(place_order)
+waiter.take_command(cancel_order)
+
+
+
