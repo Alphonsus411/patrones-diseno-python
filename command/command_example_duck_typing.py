b2e757036880d50cb25f7479d098a196775bc202
Adolfo Gonzalez Hernandez <adolfo.gonzalez@genetsis.com>
2026-07-02 12:52:30 +0200

    Añadimos nuevos ejemplos del patrón command

---
+++ command/command_example_duck_typing.py
@@ -0,0 +1,39 @@
+
+
+
+class Order:
+    def place(self):
+        print("Order has been placed.")
+
+    def cancel(self):
+        print("Order has been cancelled.")
+
+
+class PlaceOrderCommand:
+    def __init__(self, order):
+        self.order = order
+
+    def execute(self):
+        self.order.place()
+
+
+class CancelOrderCommand:
+    def __init__(self, order):
+        self.order = order
+
+    def execute(self):
+        self.order.cancel()
+
+
+class Waiter:
+    def take_command(self, command):
+        command.execute()
+
+
+order = Order()
+place_order = PlaceOrderCommand(order)
+cancel_order = CancelOrderCommand(order)
+
+waiter = Waiter()
+waiter.take_command(place_order)
+waiter.take_command(cancel_order)
