2bb12a551aef573b086774252e8a9a7e45ae6592
Adolfo Gonzalez <adolfogonzal@gmail.com>
2026-07-07 14:24:16 +0200

    Creación de la carpeta /template_method con los primeros ejemplos del patrón de diseño

---
+++ template_method/template_method_duck_typing.py
@@ -0,0 +1,43 @@
+
+
+class Bank:
+    def hire_candidate(self):
+        self.receive_cv()
+        self.conduct_interview()
+        self.issue_offer()
+
+    def receive_cv(self):
+        print("Receiving CV for Bank.")
+
+    def conduct_interview(self):
+        print("Conducting interview about banking knowledge.")
+
+    def issue_offer(self):
+        print("Issuing job offer.")
+
+
+class SoftwareCompany:
+    def hire_candidate(self):
+        self.receive_cv()
+        self.conduct_interview()
+        self.conduct_skill_test()
+        self.issue_offer()
+
+    def receive_cv(self):
+        print("Receiving CV for Software Company.")
+
+    def conduct_interview(self):
+        print("Conducting technical interview.")
+
+    def conduct_skill_test(self):
+        print("Conducting coding test.")
+
+    def issue_offer(self):
+        print("Issuing job offer.")
+
+
+software_company = SoftwareCompany()
+software_company.hire_candidate()
+
+bank = Bank()
+bank.hire_candidate()
